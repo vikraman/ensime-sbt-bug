c044a237fe47465935b5dcdbe53390afeb076ada
Vikraman Choudhury <git@vikraman.org>
2016-01-24 12:41:22 -0500

    Add more plugins

--- project/FooBuild.scala
+++ project/FooBuild.scala
@@ -1,14 +1,47 @@
 import sbt._
 import Keys._
 
+import com.typesafe.sbt.SbtScalariform._
+import scalariform.formatter.preferences._
+import sbtunidoc.Plugin._, UnidocKeys._
+
 object FooBuild extends Build {
 
-  lazy val standardSettings = Defaults.defaultSettings
+  lazy val standardSettings = Defaults.defaultSettings ++ Seq(
+    organization := "foo.bar",
+    version := "1.0-SNAPSHOT",
+
+    scalaVersion := "2.11.7",
+
+    scalacOptions ++= Seq(
+      "-encoding", "UTF-8",
+      "-deprecation",
+      "-unchecked",
+      "-feature",
+      "-Xlint",
+      "-optimise",
+      "-target:jvm-1.8",
+      "-Yinline-warnings:false",
+      "-Xfatal-warnings",
+      "-Ywarn-value-discard"
+    )
+  )
+
+  lazy val formatSettings = scalariformSettings ++ Seq(
+    ScalariformKeys.preferences := ScalariformKeys.preferences.value
+      .setPreference(AlignArguments, true)
+      .setPreference(AlignParameters, true)
+      .setPreference(AlignSingleLineCaseStatements, true)
+      .setPreference(DoubleIndentClassDeclaration, true)
+      .setPreference(PlaceScaladocAsterisksBeneathSecondAsterisk, true)
+      .setPreference(PreserveDanglingCloseParenthesis, true)
+  )
 
   lazy val foo: Project = Project(
     id = "foo",
     base = file("."))
     .settings(standardSettings)
+    .settings(unidocSettings)
     .aggregate(macros, core)
 
   lazy val macros: Project = Project(
@@ -18,8 +51,9 @@ object FooBuild extends Build {
 
   lazy val core: Project = Project(
     id = "core",
-    base = file("core")
-  ).settings(standardSettings)
+    base = file("core"))
+    .settings(standardSettings)
+    .settings(formatSettings)
     .dependsOn(macros)
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,5 @@
 addSbtPlugin("org.ensime" % "ensime-sbt" % "0.3.3")
+
+addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.6.0")
+
+addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.3.3")
